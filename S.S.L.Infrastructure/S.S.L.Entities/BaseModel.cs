2978e59eb44c848c66c01d347100cd3ae2f2a488
PriscillaSam <samiduh@live.com>
2018-05-22 05:45:16 +0100

    Add an abstract base model that models can inherit from

---
+++ S.S.L.Infrastructure/S.S.L.Entities/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace S.S.L.Infrastructure.S.S.L.Entities
+{
+    public abstract class BaseModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; } 
+    }
+}

ce8ceb6e0b4349888e1138ff662dd58dec76f8ba
PriscillaSam <samiduh@live.com>
2018-05-22 23:34:46 +0100

    Add user model DTO

---
+++ S.S.L.Domain/Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S.S.L.Domain.Models
+{
+    public class UserModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string MobileNumber { get; set; }
+        public string ImageUrl { get; set; }
+        public bool EmailConfirmed { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+    }
+}

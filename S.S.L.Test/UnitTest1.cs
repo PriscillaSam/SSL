455cc76c6ed7041bf118d85c769a76b05f8c13a3
PriscillaSam <samiduh@live.com>
2018-05-27 21:01:52 +0100

    Add Test project for unit testing

---
+++ S.S.L.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace S.S.L.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

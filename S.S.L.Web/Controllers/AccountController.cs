4f447fa45a50c044fdfb829f5226ecaefafeb6ab
PriscillaSam <samiduh@live.com>
2018-06-09 09:35:35 +0100

    Implement signup functionality

--- S.S.L.Web/Controllers/AccountController.cs
+++ S.S.L.Web/Controllers/AccountController.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.Owin.Security;
+using S.S.L.Domain.Managers;
 using S.S.L.Domain.Models;
 using S.S.L.Web.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
@@ -11,9 +10,19 @@ using System.Web.Mvc;
 
 namespace S.S.L.Web.Controllers
 {
+
     [RoutePrefix("accounts")]
     public class AccountController : Controller
     {
+
+        private readonly UserManager _user;
+        private readonly LocationManager _location;
+
+        public AccountController(UserManager user, LocationManager location)
+        {
+            _user = user;
+            _location = location;
+        }
 
         [Route("login")]
         public ActionResult Login()
@@ -24,27 +33,99 @@ namespace S.S.L.Web.Controllers
         [HttpPost]
         [Route("login")]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Login(LoginViewModel model)
+        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
         {
             var auth = HttpContext.GetOwinContext().Authentication;
 
-            var user = new UserModel();
+            try
+            {
+                var user = await _user.Login(model.Email, model.Password);
+                var claims = new Claim[]
+                {
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.GivenName, user.FullName ),
+
+                };
+
+                var userIdentity = new ClaimsIdentity(claims);
+                var props = new AuthenticationProperties
+                {
+                    IsPersistent = model.RememberMe
+                };
+
+                auth.SignIn(props, userIdentity);
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+            }
             //get claims
-            var claims = new Claim[]
-            {
-                new Claim(ClaimTypes.Email, user.Email),
-                new Claim(ClaimTypes.GivenName, user.FullName ), 
-            };
 
-            var userIdentity = new ClaimsIdentity(claims);
-            var props = new AuthenticationProperties
-            {
-                IsPersistent = model.RememberMe
-            };
-
-            auth.SignIn(props ,userIdentity);
             return View();
 
         }
+
+        [Route("register")]
+        public async Task<ActionResult> SignUp()
+        {
+            await PopulateDropdown();
+            return View();
+        }
+
+        [Route("register")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> SignUp(SignUpViewModel model)
+        {
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var user = new UserModel
+                    {
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        Email = model.Email,
+                        EmploymentStatus = model.EmploymentStatus,
+                        Gender = model.Gender,
+                        Country = model.Country,
+                        State = model.State,
+                        MobileNumber = model.MobileNumber
+
+
+                    };
+
+                    var newUser = await _user.Register(user, model.Password);
+                    return RedirectToAction("Login");
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await PopulateDropdown();
+                ViewBag.Error = ex.Message;
+            }
+
+            await PopulateDropdown();
+            return View();
+
+        }
+
+
+        private async Task PopulateDropdown()
+        {
+            ViewBag.Countries = new SelectList(await _location.GetCountries(), "Name", "Name");
+            ViewBag.States = new SelectList(await _location.GetStates(1), "Name", "Name");
+
+        }
+
+
     }
 }
---
+++ S.S.L.Web/Models/SignUpViewModel.cs
@@ -0,0 +1,54 @@
+﻿using S.S.L.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace S.S.L.Web.Models
+{
+    public class SignUpViewModel : BaseModel
+    {
+
+        [Required(ErrorMessage = "please enter your first name"), Display(Name = "First Name"), MinLength(2, ErrorMessage = "first name should not be less than two characters")]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "please enter your last name"), Display(Name = "Last Name"), MinLength(2, ErrorMessage = "last name should not be less than two characters")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "please enter a valid email address"), EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "please enter a valid phone number"), Display(Name = "Mobile Number")]
+        public string MobileNumber { get; set; }
+
+        [Required(ErrorMessage = "select your state")]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "select your country")]
+        public string Country { get; set; }
+
+        [Required, Display(Name = "Employment status")]
+        public string EmploymentStatus { get; set; }
+
+        [Required(ErrorMessage = "please enter your password"), DataType(DataType.Password), MinLength(8, ErrorMessage = "Password length should be at least 8 characters")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "please confirm your password"), DataType(DataType.Password), Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Password != ConfirmPassword)
+            {
+                yield return new ValidationResult("Passwords do not match", new[] { nameof(Password), nameof(ConfirmPassword) });
+
+            }
+        }
+    }
+
+
+}

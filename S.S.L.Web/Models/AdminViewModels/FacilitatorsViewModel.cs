e51840706571bf76d522e00828e3ca6e9108b6a5
PriscillaSam <samiduh@live.com>
2018-06-26 21:42:16 +0100

    Add functionality to manage mentees and facilitators

---
+++ S.S.L.Web/Models/AdminViewModels/FacilitatorsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using S.S.L.Domain.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace S.S.L.Web.Models.AdminViewModels
+{
+    public class FacilitatorsViewModel
+    {
+        public IEnumerable<UserModel> Facilitators { get; set; }
+
+
+        //Fields for facilitator signup
+        [Required(ErrorMessage = "please enter first name"), Display(Name = "First Name"), MinLength(2, ErrorMessage = "first name can't be less than two characters ")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "please enter last name"), Display(Name = "Last Name"), MinLength(2, ErrorMessage = "last name can't be less than two characters ")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "please enter valid email address"), EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Make Admin")]
+        public bool MakeAdmin { get; set; }
+
+    }
+}
